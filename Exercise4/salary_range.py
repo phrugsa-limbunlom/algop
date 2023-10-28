f20e091d135288f65a487e0f13047ea2b3f3fc41
Gift-eiei <gift15293@gmail.com>
2023-10-28 20:38:23 +0100

    update exercise 5

--- Exercise4/salary_range.py
+++ Exercise4/salary_range.py
@@ -1,4 +1,4 @@
-def find_salaries_range(employees, minimum_salary, maximum_salary):
+def filter_employees_by_salaries(employees, minimum_salary, maximum_salary):
     filtered_employees = []
     for employee in employees:
         if minimum_salary <= int(employee[2]) <= maximum_salary:
@@ -7,7 +7,7 @@ def find_salaries_range(employees, minimum_salary, maximum_salary):
     return filtered_employees
 
 
-def sort_salaries(employees):
+def sort_employees_by_salaries(employees):
     for i in range(len(employees)):
         for j in range(i):
             if employees[i][2] >= employees[j][2]:
@@ -37,9 +37,10 @@ if __name__ == "__main__":
                     if salary_range == "exit":
                         break
                     min_salary, max_salary = salary_range.split("-")
-                    filtered_employee_list = find_salaries_range(employee_list, int(min_salary), int(max_salary))
-                    final_employee_list = format_employees_result(sort_salaries(filtered_employee_list))
-                    print(final_employee_list)
+                    filtered_employee_list = filter_employees_by_salaries(employee_list, int(min_salary), int(max_salary))
+                    sorted_filtered_employee_list = sort_employees_by_salaries(filtered_employee_list)
+                    final_employee_list = format_employees_result(sorted_filtered_employee_list)
+                    print(f"List of employees whose salaries are in the range\n{final_employee_list}")
                 break
         except FileNotFoundError:
             print("Incorrect file name!")
